09bcefc43e22660cf02dda7793b055024a68a7bb
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2024-08-28 21:39:16 -0500

    video 30 parametros @input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,91 @@ import { LisadoPeliculasComponent } from './peliculas/lisado-peliculas/lisado-pe
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent  {
+export class AppComponent implements OnInit {
+  ngOnInit(): void {
+
+    setTimeout(() => {
+
+      this.peliculasEnCines =  [{
+        titulo: 'Inside Out 2',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+      },
+      {
+        titulo: 'Moana 2',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+      },
+      {
+        titulo: 'Bad Boys: Ride or Die',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+      },
+      {
+        titulo: 'Deadpool & Wolverine',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
+      },
+      {
+        titulo: 'Oppenheimer',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
+      },
+      {
+        titulo: 'The Flash',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
+      }];
+
+      this.peliculasProximosEstrenos =  [{
+        titulo: 'Inside Out 2',
+        fechaLanzamiento: new Date(),
+        precio: 1400.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+      },
+      {
+        titulo: 'Moana 2',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+      },
+      {
+        titulo: 'Bad Boys: Ride or Die',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+      },
+      {
+        titulo: 'Deadpool & Wolverine',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
+      },
+      {
+        titulo: 'Oppenheimer',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
+      },
+      {
+        titulo: 'The Flash',
+        fechaLanzamiento: new Date('2016-05-03'),
+        precio: 300.99,
+        poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
+      }];
+
+    }, 2000);
+  }
+  
+
+peliculasEnCines!: any[];
+peliculasProximosEstrenos!: any[];
 
   title = 'mi-aplicacion-angular';
 
--- src/app/peliculas/lisado-peliculas/lisado-peliculas.component.ts
+++ src/app/peliculas/lisado-peliculas/lisado-peliculas.component.ts
@@ -1,5 +1,5 @@
 import { CurrencyPipe, DatePipe, NgFor, NgIf, NgOptimizedImage, UpperCasePipe } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-lisado-peliculas',
@@ -13,50 +13,85 @@ export class LisadoPeliculasComponent implements OnInit {
 
     setTimeout(() => {
 
-      this.peliculas = this.DevolverPeliculas()
+      // this.peliculas =  [{
+      //   titulo: 'Inside Out 2',
+      //   fechaLanzamiento: new Date(),
+      //   precio: 1400.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+      // },
+      // {
+      //   titulo: 'Moana 2',
+      //   fechaLanzamiento: new Date('2016-05-03'),
+      //   precio: 300.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+      // },
+      // {
+      //   titulo: 'Bad Boys: Ride or Die',
+      //   fechaLanzamiento: new Date('2016-05-03'),
+      //   precio: 300.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+      // },
+      // {
+      //   titulo: 'Deadpool & Wolverine',
+      //   fechaLanzamiento: new Date('2016-05-03'),
+      //   precio: 300.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
+      // },
+      // {
+      //   titulo: 'Oppenheimer',
+      //   fechaLanzamiento: new Date('2016-05-03'),
+      //   precio: 300.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
+      // },
+      // {
+      //   titulo: 'The Flash',
+      //   fechaLanzamiento: new Date('2016-05-03'),
+      //   precio: 300.99,
+      //   poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
+      // }];
     }, 2000);
-
-
   }
   
+
+  @Input({required: true})
   peliculas!: any[]
 
-  DevolverPeliculas(): any {
-    return [{
-      titulo: 'Inside Out 2',
-      fechaLanzamiento: new Date(),
-      precio: 1400.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
-    },
-    {
-      titulo: 'Moana 2',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
-    },
-    {
-      titulo: 'Bad Boys: Ride or Die',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
-    },
-    {
-      titulo: 'Deadpool & Wolverine',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
-    },
-    {
-      titulo: 'Oppenheimer',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
-    },
-    {
-      titulo: 'The Flash',
-      fechaLanzamiento: new Date('2016-05-03'),
-      precio: 300.99,
-      poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
-    }];
-  }
+  // DevolverPeliculas(): any {
+  //   return [{
+  //     titulo: 'Inside Out 2',
+  //     fechaLanzamiento: new Date(),
+  //     precio: 1400.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832'
+  //   },
+  //   {
+  //     titulo: 'Moana 2',
+  //     fechaLanzamiento: new Date('2016-05-03'),
+  //     precio: 300.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg'
+  //   },
+  //   {
+  //     titulo: 'Bad Boys: Ride or Die',
+  //     fechaLanzamiento: new Date('2016-05-03'),
+  //     precio: 300.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg'
+  //   },
+  //   {
+  //     titulo: 'Deadpool & Wolverine',
+  //     fechaLanzamiento: new Date('2016-05-03'),
+  //     precio: 300.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg'
+  //   },
+  //   {
+  //     titulo: 'Oppenheimer',
+  //     fechaLanzamiento: new Date('2016-05-03'),
+  //     precio: 300.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg'
+  //   },
+  //   {
+  //     titulo: 'The Flash',
+  //     fechaLanzamiento: new Date('2016-05-03'),
+  //     precio: 300.99,
+  //     poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg'
+  //   }];
+  // }
 }
