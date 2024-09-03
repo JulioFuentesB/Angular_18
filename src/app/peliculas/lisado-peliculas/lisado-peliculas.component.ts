0752077a141575bb265e2a0eb5a87b69ecef166b
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2024-09-02 21:58:45 -0500

    v35 instalar angular materias

--- src/app/peliculas/lisado-peliculas/lisado-peliculas.component.ts
+++ src/app/peliculas/lisado-peliculas/lisado-peliculas.component.ts
@@ -1,11 +1,13 @@
 import { CurrencyPipe, DatePipe, NgFor, NgIf, NgOptimizedImage, UpperCasePipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { ListadoGenericoComponent } from "../../compartidos/componentes/listado-generico/listado-generico.component";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-lisado-peliculas',
   standalone: true,
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage, NgIf, ListadoGenericoComponent],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, NgFor, NgOptimizedImage, NgIf, ListadoGenericoComponent, MatButtonModule, MatIconModule],
   templateUrl: './lisado-peliculas.component.html',
   styleUrl: './lisado-peliculas.component.css'
 })
