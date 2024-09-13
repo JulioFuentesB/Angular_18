cc9452ae4f0de59b8419a3be056a2f70b6e3313c
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2024-09-12 20:09:38 -0500

    video 51 nuestro primer formulario

--- src/app/generos/crear-generos/crear-generos.component.ts
+++ src/app/generos/crear-generos/crear-generos.component.ts
@@ -1,22 +1,33 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatBadgeModule } from '@angular/material/badge';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-crear-generos',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink, MatFormFieldModule, ReactiveFormsModule, MatInputModule],
   templateUrl: './crear-generos.component.html',
   styleUrl: './crear-generos.component.css'
 })
 export class CrearGenerosComponent {
 
-  router= inject(Router);
+  router = inject(Router);
 
-  guardarCambios(){
+  private fromBuilder = inject(FormBuilder);
 
 
-    this.router.navigate(['/generos']); 
+  form = this.fromBuilder.group({
+
+    nombre: ['']
+
+  });
+
+  guardarCambios() {
+    //this.router.navigate(['/generos']);
+    console.log(this.form.value);
   }
 }
