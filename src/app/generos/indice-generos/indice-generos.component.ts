4ed6118b57db5762f29b1877432fe98bb807f88d
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2024-10-30 20:46:44 -0500

    video 105 indice de generos

--- src/app/generos/indice-generos/indice-generos.component.ts
+++ src/app/generos/indice-generos/indice-generos.component.ts
@@ -3,24 +3,29 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { GenerosService } from '../generos.service';
 import { environment } from '../../../environments/environment.development';
+import { GeneroDto } from '../generos';
+import { ListadoGenericoComponent } from '../../compartidos/componentes/listado-generico/listado-generico.component';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-indice-generos',
   standalone: true,
-  imports: [RouterLink, MatButtonModule],
+  imports: [RouterLink, MatButtonModule, ListadoGenericoComponent, MatTableModule],
   templateUrl: './indice-generos.component.html',
   styleUrl: './indice-generos.component.css'
 })
 export class IndiceGenerosComponent {
-
+  //estamosEnProduccion= environment.production;
   generosServices = inject(GenerosService);
-
-//estamosEnProduccion= environment.production;
+  generos!: GeneroDto[];
+  columnasAMostrar = ['id', 'nombre', 'acciones']
 
   constructor() {
 
     this.generosServices.ObtenerTodos().subscribe(generos => {
       console.log(generos);
+
+      this.generos = generos;
     });
 
   }
